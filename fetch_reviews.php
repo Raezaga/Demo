878416c410e58c9096e1cbc76fae7583c7f0de62
Raezaga <108203922+Raezaga@users.noreply.github.com>
2026-02-24 18:45:42 +0800

    Update fetch_reviews.php

--- fetch_reviews.php
+++ fetch_reviews.php
@@ -10,9 +10,7 @@ foreach ($reviews as $row) {
 <div class="review-item">
     <h4>
         <?php echo htmlspecialchars($row['name']); ?>
-        <span style="color:#facc15;">
-            (<?php echo htmlspecialchars($row['company']); ?>)
-        </span>
+        <span>(<?php echo htmlspecialchars($row['company']); ?>)</span>
     </h4>
 
     <p>"<?php echo htmlspecialchars($row['review']); ?>"</p>
