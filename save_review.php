fd4540be611df6c10f3a0ff41e9fd6ea3c2c0fcd
Raezaga <108203922+Raezaga@users.noreply.github.com>
2026-02-24 20:32:31 +0800

    Update save_review.php

--- save_review.php
+++ save_review.php
@@ -1,35 +1,22 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 include "config.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-$name = $_POST['name'] ?? null;
-$company = $_POST['company'] ?? null;
-$review = $_POST['review'] ?? null;
-
-if(!$name || !$company || !$review){
-    die("Missing form data");
-}
+$name = $_POST['name'];
+$company = $_POST['company'];
+$review = $_POST['review'];
 
 $stmt = $pdo->prepare("INSERT INTO reviews (name, company, review)
 VALUES (:name, :company, :review)");
 
-$execute = $stmt->execute([
+$stmt->execute([
     ':name' => $name,
     ':company' => $company,
     ':review' => $review
 ]);
 
-if($execute){
-    echo "✅ Insert Successful";
-} else {
-    echo "❌ Insert Failed";
-}
-
-} else {
-echo "Form not submitted";
+header("Location: index.php");
+exit();
 }
 ?>
