fe258856e09527f6db3604960ae7a12c7614148b
Raezaga <108203922+Raezaga@users.noreply.github.com>
2026-02-24 18:18:31 +0800

    Update fetch_reviews.php

--- fetch_reviews.php
+++ fetch_reviews.php
@@ -1,23 +1,21 @@
 <?php
 include "config.php";
 
-$result = pg_query($conn, "SELECT * FROM reviews ORDER BY created_at DESC");
+$stmt = $pdo->query("SELECT * FROM reviews ORDER BY created_at DESC");
+$reviews = $stmt->fetchAll();
 
-while($row = pg_fetch_assoc($result)){
+foreach ($reviews as $row) {
 ?>
 
 <div class="review-item">
-    <h4><?php echo $row['name']; ?>
+    <h4>
+        <?php echo htmlspecialchars($row['name']); ?>
         <span style="color:#facc15;">
-            (<?php echo $row['company']; ?>)
+            (<?php echo htmlspecialchars($row['company']); ?>)
         </span>
     </h4>
 
-    <p style="margin-top:10px;color:#cbd5e1;">
-        "<?php echo $row['review']; ?>"
-    </p>
+    <p>"<?php echo htmlspecialchars($row['review']); ?>"</p>
 </div>
 
-<?php
-}
-?>
+<?php } ?>
